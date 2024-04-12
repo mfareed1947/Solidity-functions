6773bf4aa69cfb4f9902b8b76105442ebf8c05e6
Muhammad Fareed <81671003+mfareed1947@users.noreply.github.com>
2024-04-12 12:27:49 +0500

    change version of solidity

--- function syntax/SimpleFunc.sol
+++ function syntax/SimpleFunc.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.19;
+pragma solidity 0.8.25;
 
 contract SimpleFunc {
     // when we create a public state variable we do not need to create a getter Function
